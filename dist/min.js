ce13b54df8c09703794f5963fd304c07238de47f
aileen santos <asantos3026@gmail.com>
2016-08-10 10:03:27 -0700

    min done.

--- dist/min.js
+++ dist/min.js
@@ -3,6 +3,15 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var min = function min() {};
+var min = function min(collection, iteratee) {
+  for (var key in collection) {
+    var minVal = [3];
+    if (collection[key] < minVal) {
+      minVal = collection[key];
+      collection.push(minVal);
+    }
+    return minVal;
+  }
+};
 
 exports.min = min;
--- src/min.js
+++ src/min.js
@@ -1,5 +1,14 @@
-const min = () => {
-
+const min = ( collection, iteratee ) => {
+   for( let key in collection ) {
+    let minVal = [3];
+    if (collection[key] < minVal) {
+    minVal = collection[key];
+    collection.push(minVal)
+   }
+return minVal
+ }
 }
+
+
 
 export { min }
