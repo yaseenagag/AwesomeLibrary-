79d3a3cd655257af28d8e40a7dd79c1ec67e9edb
aileen santos <asantos3026@gmail.com>
2016-08-10 10:11:15 -0700

    almost done

--- dist/max.js
+++ dist/max.js
@@ -3,6 +3,14 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var max = function max() {};
-
+var max = function max(collection, iteratee) {
+  for (var key in collection) {
+    var maxVal = [];
+    if (collection[key] > maxVal) {
+      maxVal = collection[key];
+      collection.push(maxVal);
+    }
+    return maxVal;
+  }
+};
 exports.max = max;
--- src/max.js
+++ src/max.js
@@ -1,5 +1,11 @@
-const max = () => {
-
+const max = ( collection, iteratee ) => {
+   for( let key in collection ) {
+    let maxVal = [];
+    if (collection[key] > maxVal) {
+    maxVal = collection[key];
+    collection.push(maxVal)
+   }
+return maxVal
+ }
 }
-
 export { max }
