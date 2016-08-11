cb6496b041dc37d034448f9d9ba3428191508fbd
aileen santos <asantos3026@gmail.com>
2016-08-11 15:34:00 -0700

    omit tested and completed

--- dist/omit.js
+++ dist/omit.js
@@ -3,6 +3,23 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var omit = function omit() {};
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var omit = function omit(object, attribute) {
+  if (attribute === null || attribute === undefined) {
+    return {};
+  } else if (object instanceof Array) {
+    return [].concat(_toConsumableArray(object.slice(0, attribute)), _toConsumableArray(object.slice(attribute + 1)));
+  } else {
+    for (var key in object) {
+      if (key == attribute) {
+        delete object[key];
+      }
+    }
+  }
+
+  return object;
+};
 
 exports.omit = omit;
--- test/omit.js
+++ test/omit.js
@@ -6,4 +6,22 @@ import { omit } from '../src/omit'
 
 describe( 'omit', () => {
 
+  it( 'deletes the specified key in the input object', () => {
+    const input = {first: 'shaka', last: 'lee'}
+
+    expect( omit(input, 'first')).toEqual({last:'lee'})
+  })
+
+  it( 'deletes the specified index in the input array', () => {
+    const input = [ 1, 2, 3 ]
+    expect( omit( input, 0 )).toEqual( [2, 3] )
+  })
+
+  it( 'returns an empty object for undefined input', () => {
+    expect( omit( undefined )).toEqual( {} )
+  })
+
+  it( 'returns an empty object for null input', () => {
+    expect( omit( null )).toEqual( {} )
+  })
 })
