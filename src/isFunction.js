4e4a47d5087babd991d72c2ef6a0df503bef8c0c
aileen santos <asantos3026@gmail.com>
2016-08-11 10:57:34 -0700

    Is function tests all passing

--- src/isFunction.js
+++ src/isFunction.js
@@ -1,11 +1,4 @@
-const isFunction = ( fn ) => {
-  if ( typeof fn == 'undefined' ) return false
-  if ( (fn instanceof Array ) ) return false
-  if ( (fn instanceof Object ) ) return false
-  if ( typeof fn === 'string' || fn instanceof String ) return false
-  if ( typeof fn == ( () => {} ) ) return true
-  return false
-}
+const isFunction = fn => typeof( fn ) === 'function'
 
 export { isFunction }
 
