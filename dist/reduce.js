256e333d7c2c806d84855d16e18a1148ee242d7a
aileen santos <asantos3026@gmail.com>
2016-08-10 17:48:38 -0700

    working on reduce

--- dist/reduce.js
+++ dist/reduce.js
@@ -3,6 +3,22 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var reduce = function reduce() {};
+var reduce = function reduce(input, iteratee, memo) {
+  if (!input) {
+    return memo;
+  }
+
+  var result = memo;
+
+  for (var key in input) {
+    if (result) {
+      result = iteratee(result, input[key], key, input);
+    } else {
+      result = input[key];
+    }
+  }
+
+  return result;
+};
 
 exports.reduce = reduce;
--- test/reduce.js
+++ test/reduce.js
@@ -4,9 +4,52 @@ import expect from 'expect'
 
 import { reduce } from '../src/reduce'
 
-const identity = element => element
-
-
 describe( 'reduce', () => {
+  it( 'boils down a list of values into a single value, starting with memo', () => {
+    expect( reduce( [1,2,3], (memo, value) => memo + value, 0 ) ).toEqual( 6 )
+  })
 
+  it( 'boils down a list of values into a single value, starting with first element if no memo', () => {
+    expect( reduce( [1,2,3], (memo, value) => memo + value )).toEqual( 6 )
+  })
+
+  it( 'invokes iteratee with memo, value, index, list if array', () => {
+    const test = (memo, value, index, list) => {
+      console.log( memo, value, index, list )
+      return memo + `${memo}-${value}-${index}-${list[index]}`
+    }
+
+    // 1st pass:
+    // memo: ''
+    // value: 1
+    // index: 0
+    // list[0]: 1
+    // return '-1-0-1'
+
+    // 2nd pass:
+    // memo: '-1-0-1'
+    // value: 2
+    // index: 1
+    // list[1]: 2
+    // return '-1-0-1-1-0-1-2-1-2'
+
+    expect( reduce( [1,2], test, '' )).toEqual( '-1-0-1-1-0-1-2-1-2' )
+  })
+
+  it( 'invokes iteratee with memo, value, key, object if object', () => {
+    const test = (memo, value, key, object) => {
+      console.log( memo, value, key, object )
+      return memo + `${memo}-${value}-${key}-${object[key]}`
+    }
+
+    expect( reduce( {a: 1, b: 2 }, test, '' )).toEqual( '-1-a-1-1-a-1-2-b-2' )
+  })
+
+  it( 'returns the memo if null input', () => {
+    expect( reduce( null, a => a, 'A')).toEqual( 'A' )
+  })
+
+  it( 'returns the memo if undefined input', () => {
+    expect( reduce( undefined, a => a, 'A')).toEqual( 'A' )
+  })
 })
