2cd5d35b10a2eaa27227b7b2e158f7458120cb55
aileen santos <asantos3026@gmail.com>
2016-08-11 09:22:27 -0700

    finished isString

--- test/isString.js
+++ test/isString.js
@@ -6,4 +6,23 @@ import { isString } from '../src/isString'
 
 describe( 'isString', () => {
 
+  it( 'returns true if input is a string', () => {
+    expect( isString( 'love' ) ).toEqual(true)
+  })
+
+  it( 'returns false if input is an array', () => {
+    expect( isString( [1,2,3] ) ).toEqual(false)
+  })
+
+  it( 'returns false if input is null', () => {
+    expect( isString( null ) ).toEqual(false)
+  })
+
+  it( 'returns false if input is undefined', () => {
+    expect( isString( undefined ) ).toEqual(false)
+  })
+
+  it( 'returns false if input is an object', () => {
+    expect( isString( {name: 'Shaka'}) ).toEqual(false)
+  })
 })
