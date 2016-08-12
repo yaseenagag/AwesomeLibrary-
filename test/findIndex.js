3ebb2689861bbe755bfe2271cfe04c6cca6aaa32
yaseenagag <yaseen.uc@gmail.com>
2016-08-12 12:21:54 -0700

    fixed merge conflicts in test/findIndex.js

--- test/findIndex.js
+++ test/findIndex.js
@@ -7,7 +7,6 @@ import { findIndex } from '../src/findIndex'
 describe( 'findIndex', () => {
 
   it( 'returns -1 when no values pass the truth test', () => {
-<<<<<<< HEAD
   	const isPrime = (number) => {
   		let start = 2;
   		while(start <= Math.sqrt(number)) {
@@ -15,21 +14,12 @@ describe( 'findIndex', () => {
   		}
   		return number > 1
   	}
-=======
-    const isPrime = (number) => {
-      let start = 2;
-      while(start <= Math.sqrt(number)) {
-        if (number % start++ < 1) return false
-      }
-      return number > 1
-    }
->>>>>>> 7dde6be2ddf1bc1584b19b411f408b35d4e0edef
+
     const input = [4, 6, 8, 12]
 
     expect( findIndex(input, isPrime )).toEqual(-1)
   })
 
-<<<<<<< HEAD
 
   it( 'returns the index of the first value that passes the truth test', () => {
   	const isPrime = (number) => {
@@ -39,23 +29,10 @@ describe( 'findIndex', () => {
   		}
   		return number > 1
   	}
-=======
-    it( 'returns -1 when no values pass the truth test', () => {
-    const isPrime = (number) => {
-      let start = 2;
-      while(start <= Math.sqrt(number)) {
-        if (number % start++ < 1) return false
-      }
-      return number > 1
-    }
->>>>>>> 7dde6be2ddf1bc1584b19b411f408b35d4e0edef
+
     const input = [4, 6, 7, 12]
 
     expect( findIndex(input, isPrime )).toEqual(2)
   })
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 7dde6be2ddf1bc1584b19b411f408b35d4e0edef
 })
