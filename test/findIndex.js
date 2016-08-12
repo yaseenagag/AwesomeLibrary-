5f234811afa0fce1a4ca0f6f4d6d86831d7d4301
aileen santos <asantos3026@gmail.com>
2016-08-12 11:36:03 -0700

    Fixed bug in findIndex

--- test/findIndex.js
+++ test/findIndex.js
@@ -7,28 +7,30 @@ import { findIndex } from '../src/findIndex'
 describe( 'findIndex', () => {
 
   it( 'returns -1 when no values pass the truth test', () => {
-  	const isPrime = (number) => {
-  		let start = 2;
-  		while(start <= Math.sqrt(number)) {
-  			if (number % start++ < 1) return false
-  		}
-  		return number > 1
-  	}
+    const isPrime = (number) => {
+      let start = 2;
+      while(start <= Math.sqrt(number)) {
+        if (number % start++ < 1) return false
+      }
+      return number > 1
+    }
     const input = [4, 6, 8, 12]
 
     expect( findIndex(input, isPrime )).toEqual(-1)
   })
 
+    it( 'returns -1 when no values pass the truth test', () => {
+    const isPrime = (number) => {
+      let start = 2;
+      while(start <= Math.sqrt(number)) {
+        if (number % start++ < 1) return false
+      }
+      return number > 1
+    }
+    const input = [4, 6, 7, 12]
 
-//   it( 'returns -1 when the value is not present', () => {
+    expect( findIndex(input, isPrime )).toEqual(2)
+  })
 
-//     expect( findIndex([4,6,8,12], fn(number){
-//     		let start = 2
-//     		while (start <= Math.sqrt(number)) {
-//     			if (number % start++ < 1) return false
-//     		}
-//     	return number > 1
-//     	})
 
-//   } ).toEqual(-1)
 })
